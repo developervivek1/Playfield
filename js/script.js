e88f97e032f07ce1305deac9e6525344aeccc319
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-05-09 11:22:18 +0530

    playfield repo updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    var element = document.getElementById("mySidenav");
+    element.classList.add("mystyle");
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
     document.getElementById("burger").style.display = "none";
     document.getElementById("burger2").style.display = "block";
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "50px";
-    document.getElementById("main").style.marginLeft= "50px";
-     document.getElementById("burger2").style.display = "none";
-     document.getElementById("burger").style.display = "block";
+function closeNav() {
+    var element = document.getElementById("mySidenav");
+    element.classList.remove("mystyle");
+    document.getElementById("mySidenav").style.width = "80px";
+    document.getElementById("main").style.marginLeft= "80px";
+    document.getElementById("burger2").style.display = "none";
+    document.getElementById("burger").style.display = "block";
   }
+
+
+function openSearch(){
+  var element = document.getElementById("search2");
+  element.classList.toggle("form-search")
+  document.getElementById("search2").style.transition = "ease-in .2s";
+}  
